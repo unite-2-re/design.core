5f361de25e22b0c7c6f1a97009b9c158fbd385c1
unit-404 <conway24b@gmail.com>
2024-11-08 16:29:10 +0700

    better shape

--- src/$core$/Shape.ts
+++ src/$core$/Shape.ts
@@ -1,19 +1,19 @@
 // Currently, I'm using SASS version of such p.sheet
-export const WavyShapedCircle = (steps = 100, amplitude = 0.06, freq = 8) => {
+export const WavyShapedCircle = (steps = 100, amplitude = 0.05, freq = 8) => {
     const points: number[] = [];
     for (let i = 0; i < steps; i++) { points.push(i / steps); }
 
     //
     const angle = (step) => { return `calc(${step}rad * pi * 2)`; };
     const variant = (step) => {
-        return `calc(calc(cos(calc(var(--clip-freq) * ${angle(step)})) * 0.5 + 0.5) * var(--clip-amplitude))`;
+        return `calc(calc(cos(calc(var(--clip-freq, 8) * ${angle(step)})) * 0.5 + 0.5) * var(--clip-amplitude, 0))`;
     };
 
     //
     const func = (step) => {
         return [
-            `calc(calc(0.5 + calc(cos(${angle(step)}) * calc(0.5 - ${variant(step)}))) * 100%)`,
-            `calc(calc(0.5 + calc(sin(${angle(step)}) * calc(0.5 - ${variant(step)}))) * 100%)`
+            `calc(calc(0.5 + calc(cos(${angle(step)}) * calc(0.5 - ${variant(step)}))) * var(--icon-size. 100%))`,
+            `calc(calc(0.5 + calc(sin(${angle(step)}) * calc(0.5 - ${variant(step)}))) * var(--icon-size. 100%))`
         ];
     };
 
